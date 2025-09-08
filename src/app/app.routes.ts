c9cdced0814b455623c9ab314993753bfc26ad77
Almodhesh Plus <almodheshplus.com@gmail.com>
2025-09-08 15:31:45 +0300

    adding files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "user/home",
+    component: HomeComponent
+  },
+  {
+    path: "user/about",
+    component: AboutComponent
+  },
+  {
+    path: "user/login",
+    component: LoginComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "user/home"
+  }
+];
